b0b8fe4d64f445c6bb58e60ab75558a077359315
MohammadAnwarTabbaa <anwar.tabbaa94@gmail.com>
2021-06-14 12:03:02 +0300

    step 4

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 const today = new Date();
-const time = today.getHours()+":"+today.getMinutes();
+const time = today.getHours() + ":" + today.getMinutes();
 
-app.get('/', (req, res) => {
-  res.send('ok')
-})
-app.get('/test', (req, res) => {
-    res.send({status:200, message:"ok"})
-  })
-  app.get('/time', (req, res) => {
-     
-      res.send({status:200 ,message:time });
-  })
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+app.get("/test", (req, res) => {
+  res.status(200).send({ status: 200, message: "ok" });
+});
+app.get("/time", (req, res) => {
+  res.status(200).send({ status: 200, message: time });
+});
+app.get("/hello/:id", (req, res) => {
+  res.status(200).send({ status: 200, message: "hello " + req.params.id });
+});
+app.get("/search", (req, res) => {
+  const search = req.query.s;
+  if (search) {
+    res.status(200).send({ status: 200, message: "ok", data: search });
+  } else {
+    res.status(500).send({
+      status: 500,
+      error: true,
+      message: "you have to provide a search",
+    });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
